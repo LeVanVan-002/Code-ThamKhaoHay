3a12aafb2e63596ee8db4c4f0782f28793fcaab7
LeVanVan-002 <vanduoitom202@gmail.com>
2022-06-06 11:14:40 +0700

    first commit

---
+++ loginagularjs/js.js
@@ -0,0 +1,18 @@
+angular.module('hello', [ 'ngRoute' ])
+  .config(function($routeProvider) {
+ 
+$routeProvider.when('/', {
+templateUrl : 'home.html',
+controller : 'home'
+}).when('/login', {
+templateUrl : 'login.html',
+controller : 'navigation'
+}).otherwise('/');
+ 
+  })
+  .controller('home', function($scope, $http) {
+    $http.get('/resource/').success(function(data) {
+      $scope.greeting = data;
+    })
+  })
+  .controller('navigation', function() {});
